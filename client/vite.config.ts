67889dcc1f33e6db6e39adacdff35d1308ec0771
Krzysiek <krzysztofu1024@gmail.com>
2026-02-03 16:58:19 +0100

    docs: update README

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
+import vueDevTools from 'vite-plugin-vue-devtools'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import path from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vueDevTools()],
   build: {
     outDir: path.resolve(__dirname, '../server/dist/public'),
     emptyOutDir: true,
